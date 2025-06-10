7b3ff23189f832b13de3e503c2a9c200f42d37a6
satriowaskitho <satriowaskitho@gmail.com>
2025-06-10 09:18:59 +0700

    add option value on tahun id

--- core/app/Views/layout.php
+++ core/app/Views/layout.php
@@ -187,9 +187,11 @@
                                         <label for="tahun-select2" class="color-white">Tahun</label>
                                         <select class="form-control" id="tahun">
                                             <option disabled="" selected="">Pilih</option>
+                                            <option value="2025">2025</option>
+                                            <option value="2024">2024</option>
+                                            <option value="2023">2023</option>
                                             <option value="2022">2022</option>
                                             <option value="2021">2021</option>
-                                       
                                         </select>
                                     </div>
                                 </div>
